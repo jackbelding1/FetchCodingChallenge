d656b453c83f14e3aba59078bc86c940ac848f0e
jackbelding1 <jack_belding@yahoo.com>
2023-11-03 15:17:23 -0400

    add project

---
+++ FetchCodingChallenge/FetchCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchCodingChallengeApp.swift
+//  FetchCodingChallenge
+//
+//  Created by Jack Belding on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
